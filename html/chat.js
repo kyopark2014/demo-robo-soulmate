f56dc27a1f45913146630b37060d5858aaf8edbc
ksdyb.park <ksdyb@amazon.com>
2024-03-22 09:25:16 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -364,10 +364,6 @@ function loadAudio(requestId, text) {
 
     let langCode = 'ko-KR';  // ko-KR en-US(영어)) ja-JP(일본어)) cmn-CN(중국어)) sv-SE(스페인어))
 
-    if (conversationType=='trnaslation') {  // To-Do
-
-    }
-
     xhr.open("POST", uri, true);
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4 && xhr.status === 200) {
