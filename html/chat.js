6ebb7e299267f18a06490a63be892a35625a970b
ksdyb.park <ksdyb@amazon.com>
2024-04-05 16:06:42 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -1,6 +1,6 @@
 const protocol = 'WEBSOCKET'; // WEBSOCKET 
 const langstate = 'korean'; // korean or english
-const enableTTS = true;
+const enableTTS = false;
 const enableDelayedMessage = true; // in order to manipulate the voice messages
 
 // Common
