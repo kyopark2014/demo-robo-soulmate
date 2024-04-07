81cd302e74338b998edfb6e49a51894efec29700
ksdyb.park <ksdyb@amazon.com>
2024-04-08 01:22:45 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -923,11 +923,12 @@ function getScore(userId, requestId, text) {
             let result = response.result;
             console.log("result: " + JSON.stringify(result));   
             let score = result.score;
-            console.log("score: " + score);    
+            console.log("score: ", score);    
             let description = result.description;
-            console.log("description: " + description);    
+            console.log("description: ", description);    
 
-            addNotifyMessage('[debug] score: '+score+', description: '+description);
+            let noti = '[debug] score: '+score+', description: '+description;
+            addNotifyMessage(noti);
             
             if(speechType=='robot' || speechType=='both') {
                 sendControl(userId, "action", "", score, requestId)
