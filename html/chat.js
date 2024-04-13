1ce002a40e3ae4b03011f978d68aba55920e5d91
ksdyb.park <ksdyb@amazon.com>
2024-04-13 14:18:56 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -987,7 +987,7 @@ function addReceivedMessage(requestId, msg) {
     // console.log("add received message: "+msg);
     sender = "Chatbot"
 
-    console.log('requestId: '+requestId+ '(index: '+indexList.get(requestId+':receive')+')')
+    // console.log('requestId: '+requestId+ '(index: '+indexList.get(requestId+':receive')+')')
     if (indexList.get(requestId+':receive') == undefined) {
         indexList.put(requestId+':receive', index);
         current = index;
@@ -997,7 +997,7 @@ function addReceivedMessage(requestId, msg) {
         current = indexList.get(requestId+':receive');
         // console.log("reused index="+index+', id='+requestId+':receive');
     }
-    console.log("current index:"+current+', msg='+msg);
+    // console.log("current index:"+current+', msg='+msg);
 
     msg = msg.replaceAll("\n", "<br/>");
 
