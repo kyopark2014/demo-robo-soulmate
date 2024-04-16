b0d958ce632dbae5bb8ae36a0217f8b891b67474
ksdyb.park <ksdyb@amazon.com>
2024-04-16 19:37:16 +0900

    update

--- lambda-redis/lambda_function.py
+++ lambda-redis/lambda_function.py
@@ -94,7 +94,7 @@ def lambda_handler(event, context):
         "isBase64Encoded": False,
         'statusCode': 200,
         'body': json.dumps({ 
-            "channel": f"{userId}",
+            "userId": userId,
             "state": state
         })
     }
