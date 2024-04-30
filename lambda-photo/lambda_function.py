d68f3baac2193d6c20d2fdca7754be49a9eaa67b
joonyong <joon20150101@gmail.com>
2024-04-30 13:17:38 +0900

    update

--- lambda-photo/lambda_function.py
+++ lambda-photo/lambda_function.py
@@ -56,7 +56,7 @@ def get_client(profile_of_Image_LLMs, selected_LLM):
 def img_resize(image):
     imgWidth, imgHeight = image.size 
     
-    max_length = 1024
+    max_length = 512
 
     if imgWidth < imgHeight:
         imgWidth = int(max_length/imgHeight*imgWidth)
