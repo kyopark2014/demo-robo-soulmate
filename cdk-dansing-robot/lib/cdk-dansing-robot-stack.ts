51f00858bc5031375d6b4bbbd7fdf489812019ec
Kyoungsu Park <kyopark2014@gmail.com>
2024-05-04 22:05:57 +0900

    update - add mumbai for multi-region LLM

--- cdk-dansing-robot/lib/cdk-dansing-robot-stack.ts
+++ cdk-dansing-robot/lib/cdk-dansing-robot-stack.ts
@@ -51,6 +51,12 @@ const claude3_sonnet = [
     "model_type": "claude3",
     "model_id": "anthropic.claude-3-sonnet-20240229-v1:0",
     "maxOutputTokens": "4096"
+  },
+  {
+    "bedrock_region": "ap-south-1", // Mumbai
+    "model_type": "claude3",
+    "model_id": "anthropic.claude-3-sonnet-20240229-v1:0",
+    "maxOutputTokens": "4096"
   }
 ];
 
@@ -88,6 +94,12 @@ const claude3_haiku = [
   },
   {
     "bedrock_region": "eu-west-3", // Paris
+    "model_type": "claude3",
+    "model_id": "anthropic.claude-3-haiku-20240307-v1:0",
+    "maxOutputTokens": "4096"
+  },
+  {
+    "bedrock_region": "ap-south-1", // Mumbai
     "model_type": "claude3",
     "model_id": "anthropic.claude-3-haiku-20240307-v1:0",
     "maxOutputTokens": "4096"
