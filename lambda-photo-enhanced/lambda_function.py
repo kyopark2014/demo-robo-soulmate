3bddf2daabd6420a8b58890fae1362c5f6d19037
Kyoungsu Park <kyopark2014@gmail.com>
2024-05-12 02:00:34 +0900

    update - photo (enhanced)

--- lambda-photo-enhanced/lambda_function.py
+++ lambda-photo-enhanced/lambda_function.py
@@ -422,13 +422,13 @@ def lambda_handler(event, context):
             m1 = np.all(np_mask == (0, 0, 0), axis=2)
             m2 = np.all(np_image == (0, 0, 0), axis=2)
             
-            m = np.all(np.logical_or(m1, m2), axis=2)
+            m = np.logical_or(m1, m2)
             print('m: ', m)
             
             #np_image = np.where(m1 == True, (0,0,0), (255,255,255))
             #print('np_image: ', np_image)
             
-            for i, row in enumerate(m1):
+            for i, row in enumerate(m):
                 for j, value in enumerate(row):
                     if value == True:
                         np_image[i, j] = (0, 0, 0)
