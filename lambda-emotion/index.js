c19a5d6a9c33c7e18745f457171ed0e10422343e
e-ul <98747932+e-ul@users.noreply.github.com>
2024-05-13 15:03:37 +0900

    Update index.js
    S3에 이미지 저장 및 Rekognition 결과 DDB에 저장

--- lambda-emotion/index.js
+++ lambda-emotion/index.js
@@ -4,6 +4,8 @@ const s3 = new aws.S3();
 const bucketName = process.env.bucketName;
 const collectionId = process.env.collectionId;
 
+const dynamoDB = new aws.DynamoDB.DocumentClient();
+
 exports.handler = async (event, context) => {
     // console.log('## ENVIRONMENT VARIABLES: ' + JSON.stringify(process.env));
     // console.log('## EVENT: ' + JSON.stringify(event))
@@ -66,51 +68,17 @@ exports.handler = async (event, context) => {
         };
         // console.log('rekognitionParams = '+JSON.stringify(rekognitionParams))
 
-        const data = await rekognition.detectFaces(rekognitionParams).promise();
-        // console.log('data: '+JSON.stringify(data));
+        const detectResponse = await rekognition.detectFaces(rekognitionParams).promise();
+        console.log('data: '+JSON.stringify(detectResponse));
 
-        if (data['FaceDetails'][0]) {
-            const profile = data['FaceDetails'][0];
-
-            const ageRange = profile['AgeRange'];
-            // console.log('ageRange: '+JSON.stringify(ageRange));
-            const smile = profile['Smile']['Value'];
-            // console.log('smile: ', smile);
-            const eyeglasses = profile['Eyeglasses']['Value'];
-            // console.log('smile: ', smile);
-            const sunglasses = profile['Sunglasses']['Value'];
-            // console.log('sunglasses: ', sunglasses);
-            const gender = profile['Gender']['Value'].toLowerCase();
-            // console.log('gender: ', gender);
-            const beard = profile['Beard']['Value'];
-            // console.log('beard: ', beard);
-            const mustache = profile['Mustache']['Value'];
-            // console.log('mustache: ', mustache);
-            const eyesOpen = profile['EyesOpen']['Value'];
-            // console.log('eyesOpen: ', eyesOpen);
-            const mouthOpen = profile['MouthOpen']['Value'];
-            // console.log('mouthOpen: ', mouthOpen);
-            const emotions = profile['Emotions'][0]['Type'];
-            // console.log('emotions: ', emotions);
-
-            let generation;
-            let ageRangeLow = ageRange.Low;
-            let ageRangeHigh = ageRange.High;
-            let middleAge = (ageRangeLow + ageRangeHigh) / 2;
-            if (middleAge <= 5) generation = 'toddler'; // 유아
-            else if (middleAge <= 12) generation = 'child'; // 아동
-            else if (middleAge <= 18) generation = 'teenager'; // 청소년
-            else if (middleAge <= 25) generation = 'young-adult'; // 청년
-            else if (middleAge <= 49) generation = 'adult'; // 중년
-            else if (middleAge <= 64) generation = 'middle-age'; // 장년
-            else if (middleAge >= 65) generation = 'elder'; // 노년
+        if (detectResponse['FaceDetails'][0]) {
 
             // search the face for identification
             let faceId;
+
             try {
-                const searchFacesByImageParams = {
+                const indexFacesParams = {
                     CollectionId: collectionId,
-                    FaceMatchThreshold: 80, //default
                     Image: {
                         S3Object: {
                             Bucket: bucketName,
@@ -118,70 +86,52 @@ exports.handler = async (event, context) => {
                         },
                     },
                 };
-                // console.log('rekognitionParams = '+JSON.stringify(searchFacesByImageParams))
-                const data = await rekognition.searchFacesByImage(searchFacesByImageParams).promise();
-                console.log('data: '+JSON.stringify(data));
+                // console.log('rekognitionParams = '+JSON.stringify(indexFacesParams))
+                const indexResponse = await rekognition.indexFaces(indexFacesParams).promise();
+                console.log('data: '+JSON.stringify(indexResponse));
+                
+                
+                // const primaryFaceId = indexResponse.FaceRecords[0]?.Face.FaceId; // 첫 번째 얼굴의 FaceId
+                const faceDetails = indexResponse.FaceRecords.map((record, index) => {
+                    const detail = {
+                        id: record.Face.FaceId,
+                        key: fileName,
+                        name: record.Face.FaceId, 
+                        bucket: bucketName,
+                        age: calculateMiddleAge(detectResponse.FaceDetails[index].AgeRange),
+                        emotions: detectResponse.FaceDetails[index].Emotions[0].Type,
+                        smile: detectResponse.FaceDetails[index].Smile.Value,
+                        eyeglasses: detectResponse.FaceDetails[index].Eyeglasses.Value,
+                        sunglasses: detectResponse.FaceDetails[index].Sunglasses.Value,
+                        gender: detectResponse.FaceDetails[index].Gender.Value,
+                        beard: detectResponse.FaceDetails[index].Beard.Value,
+                        mustache: detectResponse.FaceDetails[index].Mustache.Value,
+                        eyesOpen: detectResponse.FaceDetails[index].EyesOpen.Value,
+                        mouthOpen: detectResponse.FaceDetails[index].MouthOpen.Value,
+                        generation: determineGeneration(calculateMiddleAge(detectResponse.FaceDetails[index].AgeRange)),
+                        time: formatLocalDate(),
+                        createdAt: new Date(new Date().getTime() + 9 * 3600 * 1000).toISOString()
+                    };
+                    return detail;
+                });
 
-                if(!data['FaceMatches'].length) {  // if no matched face
-                    try {
-                        const indexFacesParams = {
-                            CollectionId: collectionId,
-                            Image: {
-                                S3Object: {
-                                    Bucket: bucketName,
-                                    Name: fileName
-                                },
-                            },
-                        };
-                        // console.log('rekognitionParams = '+JSON.stringify(indexFacesParams))
-                        const data = await rekognition.indexFaces(indexFacesParams).promise();
-                        console.log('data: '+JSON.stringify(data));
-
-                        faceId = data['FaceRecords'][0]['Face'].FaceId;
-                    } catch (err) {
-                        console.log(err);
-                    }
-                }
-                else { // if there is a matech image
-                    faceId = data['FaceMatches'][0]['Face'].FaceId;
-                    console.log('faceId: ', faceId);
-                }
-
-            } catch (err) {
-                console.log(err);
-            }
-
-            const emotionInfo = {
-                id: faceId,
-                bucket: bucketName,
-                key: fileName,
-                ageRange: ageRange,
-                smile: smile,
-                eyeglasses: eyeglasses,
-                sunglasses: sunglasses,
-                gender: gender,
-                beard: beard,
-                mustache: mustache,
-                eyesOpen: eyesOpen,
-                mouthOpen: mouthOpen,
-                emotions: emotions,
-                generation: generation
-            };
-            console.info('emotionInfo: ' + JSON.stringify(emotionInfo));
-
-            response = {
-                statusCode: 200,
-                body: JSON.stringify(emotionInfo)
-            };
-
-            // delete profile image
-            try {
-                let deleteParams = {  
-                    Bucket: bucketName, 
-                    Key: fileName 
+                await Promise.all(faceDetails.map(face => {
+                    const params = {
+                        TableName: "EmotionDetailInfo-3d2nq2n4sfcqnfelmjj3n3ycje-dev",
+                        Item: face
+                    };
+                    return dynamoDB.put(params).promise();
+                }));
+                
+                const emotionInfo = {
+                    key: fileName
                 };
-
-                s3.deleteObject(deleteParams).promise();
+                console.info('emotionInfo: ' + JSON.stringify(emotionInfo));
+            
+                response = {
+                    statusCode: 200,
+                    body: JSON.stringify(emotionInfo)
+                };
             } catch (err) {
                 console.log(err);
             }
@@ -202,6 +152,29 @@ exports.handler = async (event, context) => {
             body: error
         };
     }
+    
+    function calculateMiddleAge(ageRange) {
+        let ageRangeLow = ageRange.Low;
+        let ageRangeHigh = ageRange.High;
+        return (ageRangeLow + ageRangeHigh) / 2;
+    }
+    
+    function determineGeneration(middleAge) {
+        if (middleAge <= 5) return 'toddler'; // 유아
+        else if (middleAge <= 12) return 'child'; // 아동
+        else if (middleAge <= 18) return 'teenager'; // 청소년
+        else if (middleAge <= 25) return 'young-adult'; // 청년
+        else if (middleAge <= 49) return 'adult'; // 중년
+        else if (middleAge <= 64) return 'middle-age'; // 장년
+        else if (middleAge >= 65) return 'elder'; // 노년
+
+    }
+    
+    function formatLocalDate() {
+        const offset = 9; // UTC+9 for KST
+        const now = new Date(new Date().getTime() + offset * 3600 * 1000);
+        return now.toISOString().replace(/T/, ' ').replace(/\..+/, '');
+}
     
     function wait() {
         return new Promise((resolve, reject) => {
